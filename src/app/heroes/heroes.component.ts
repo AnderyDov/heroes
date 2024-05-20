8a7941dec3659952e6912c6014a3085827a7f7fa
AndreyDov <dovban.andreyy@rambler.ru>
2024-05-20 21:14:22 +0300

    add: передача сосотяния от родителя к дочке

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
 	selector: 'app-heroes',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, HeroDetailComponent],
 	templateUrl: './heroes.component.html',
 	styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm',
-	};
+	heroes: Hero[] = HEROES;
+
+	selectedHero?: Hero;
+	onSelect(hero: Hero): void {
+		this.selectedHero = hero;
+		console.log(this.selectedHero);
+	}
 }
