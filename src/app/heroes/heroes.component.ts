7b125a5ea9e8593eb92d6094872aeeff887fc5a0
AndreyDov <dovban.andreyy@rambler.ru>
2024-05-20 23:03:37 +0300

    add: работа с сервисами

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
-import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
 	selector: 'app-heroes',
@@ -11,12 +12,26 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 	templateUrl: './heroes.component.html',
 	styleUrl: './heroes.component.css',
 })
-export class HeroesComponent {
-	heroes: Hero[] = HEROES;
-
+export class HeroesComponent implements OnInit {
 	selectedHero?: Hero;
+
+	heroes: Hero[] = [];
+
+	constructor(
+		private heroService: HeroService,
+		private messageService: MessageService,
+	) {}
+
+	ngOnInit(): void {
+		this.getHeroes();
+	}
+
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
-		console.log(this.selectedHero);
+		this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+	}
+
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
 	}
 }
